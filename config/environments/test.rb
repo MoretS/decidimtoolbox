53fa464bc01b13d0a74466470b427d0319c78f98
Armand Fardeau <fardeauarmand@gmail.com>
2020-02-21 19:59:48 +0100

    Change rubytoolbox url for testing

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -3,6 +3,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.catalog_url = "https://github.com/rubytoolbox/catalog"
+
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
