7610663854bc3f45126e77a3e099450f083bd605
Armand Fardeau <fardeauarmand@gmail.com>
2020-02-24 11:46:11 +0100

    Update url

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Decidimtoolbox
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.catalog_url = "https://rubytoolbox.github.io/catalog/"
+    config.catalog_url = "https://catalog.opensourcepolitics.eu/catalog.json"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
